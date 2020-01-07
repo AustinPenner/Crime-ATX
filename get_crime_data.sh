acec6c366b4bffde6277470ec42257db07043ba3
Ubuntu <ubuntu@ip-172-31-21-43.us-east-2.compute.internal>
2020-01-07 21:14:40 +0000

    updated script for retrieving crime data

---
+++ get_crime_data.sh
@@ -0,0 +1,2 @@
+sudo wget -O police_data.csv https://data.austintexas.gov/api/views/fdj4-gpfu/rows.csv
+sudo sed -i 1d police_data.csv
--- getdata.sh
+++
@@ -1,1 +0,0 @@
-sudo wget police_data.csv https://data.austintexas.gov/api/views/fdj4-gpfu/rows.csv
